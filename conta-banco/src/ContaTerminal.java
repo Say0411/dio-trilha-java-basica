74db71303f4df1c3a47c8eb770e238f577c91a47
naral@say <sayonarajns@gmail.com>
2024-06-03 23:25:00 -0300

    Criação da ContaBanco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
-public class App {
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor,  digite o numero da conta:" );
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o numero da agência:");
+        String nuemroAgencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá," + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + nuemroAgencia + ", conta " + numeroConta + " e seu saldo $ " + saldo + " já está disponível para saque.");
+
+        
+
+        scanner.close();
+
+        
     }
 }
